8a94dbdc5c6aba0500777c03ef98f8d0c1b9a866
ji-yeonpark <happpysophiapark@gmail.com>
2019-05-24 00:16:42 +0900

    20190524 완료.
    - [#6] AJAX
    - [#7] Finishing Up
    - [#8] Building for Production

--- JavaScript/Nomad_Coders/ReactJS-Foundamentals/movie_app/src/App.js
+++ JavaScript/Nomad_Coders/ReactJS-Foundamentals/movie_app/src/App.js
@@ -7,44 +7,46 @@ class App extends Component {
   state = {
   }
 
-  componentWillMount() {
+  // componentWillMount() {
 
-  }
+  // }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "매트릭스",
-            poster: "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif"
-          },
-          {
-            title: "올드보이",
-            poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg"
-          },
-          {
-            title: "매트릭스",
-            poster: "https://upload.wikimedia.org/wikipedia/en/7/7a/Harry_Potter_and_the_Philosopher%27s_Stone_banner.jpg"
-          },
-    
-        ]
-      })
-    }, 5000)
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      return <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
     })
 
     return movies;
   }
 
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+      .then(response => response.json())  // convert ReadableStream to json
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
+  }
+
   render() {
-    console.log("did render")
+    const { movies } = this.state;
     return (
-      <div className="App">
+      <div className={movies ? "App" : "App--loading"}>
         {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     )
