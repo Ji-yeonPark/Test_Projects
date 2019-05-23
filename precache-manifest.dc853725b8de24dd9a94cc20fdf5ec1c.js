0131ef196bd47415b62079711eb5403277a8b010
ji-yeonpark <happpysophiapark@gmail.com>
2019-05-24 00:28:20 +0900

    Updates

---
+++ precache-manifest.dc853725b8de24dd9a94cc20fdf5ec1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d9221b365ce055c2c7c54f3526047d",
+    "url": "/TestProjects/index.html"
+  },
+  {
+    "revision": "9b209a49b45559bea325",
+    "url": "/TestProjects/static/css/main.b10ba9e0.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/TestProjects/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "9b209a49b45559bea325",
+    "url": "/TestProjects/static/js/main.1d3c544e.chunk.js"
+  },
+  {
+    "revision": "db892994735540f6a746",
+    "url": "/TestProjects/static/js/runtime~main.ab17b374.js"
+  }
+]);
