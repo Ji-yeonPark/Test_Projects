ef27b02baae813126536099d4e2f7427aeffead4
ji-yeonpark <happpysophiapark@gmail.com>
2019-05-28 00:42:59 +0900

    20190528(화) Vue.js Todo App - Component Communication - Part 2
    20190528(화)
    - Vue.js Todo App - Component Communication - Part 2

--- Electron/todo-vue/README.md
+++ Electron/todo-vue/README.md
@@ -1,13 +1,24 @@
 # todo-vue
 ---
 
-### :start: Electron + Vue 
+### :star: Electron + Vue 
 
 ---
 
 ## Part 1. [Vue.js Todo App - Basics - Part 1](https://www.youtube.com/watch?v=A5S23KS_-bU&list=PLEhEHUEU3x5q-xB1On4CsLPts0-rZ9oos)
 
 1) install
-  > npm i sass-loader node-sass  --save
+  > $ npm i sass-loader node-sass  --save
 
-  
+- TodoList 생성
+
+<br/>
+
+## Part 2. [Vue.js Todo App - Component Communication - Part 2](https://www.youtube.com/watch?v=4WwzOZzoUUg&list=PLEhEHUEU3x5q-xB1On4CsLPts0-rZ9oos&index=2)
+
+- TodoList 에서 TodoItem 분리
+
+<br/>
+
+## Part 3. [Vue.js Todo App - Event Bus - Part 3](https://www.youtube.com/watch?v=7AXiN5mrOgY&list=PLEhEHUEU3x5q-xB1On4CsLPts0-rZ9oos&index=3)
+
--- Electron/todo-vue/src/components/TodoList.vue
+++ Electron/todo-vue/src/components/TodoList.vue
@@ -7,29 +7,20 @@
       v-model="newTodo"
       @keyup.enter="addTodo"
     >
-    <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="adimated fadeOutDown">
-    <div v-for="(todo, index) in todosFiltered" :key="todo.id" class="todo-item">
-      <div class="todo-item-left">
-        <input type="checkbox" v-model="todo.completed">
-        <div
-          v-if="!todo.editing"
-          @dblclick="editTodo(todo)"
-          class="todo-item-label"
-          v-bind:class="{ completed : todo.completed }"
-        >{{ todo.title }}</div>
-        <input
-          v-else
-          class="todo-item-edit"
-          type="text"
-          v-model="todo.title"
-          @blur="doneEdit(todo)"
-          @keyup.enter="doneEdit(todo)"
-          @keyup.esc="cancelEdit(todo)"
-          v-focus
-        >
-      </div>
-      <div class="remove-item" @click="removeTodo(index)">&times;</div>
-    </div>
+    <transition-group
+      name="fade"
+      enter-active-class="animated fadeInUp"
+      leave-active-class="adimated fadeOutDown"
+    >
+      <todo-item
+        v-for="(todo, index) in todosFiltered"
+        :key="todo.id"
+        :todo="todo"
+        :index="index"
+        :checkAll="!anyRemaining"
+        @removedTodo="removeTodo"
+        @finishedEdit="finishedEdit"
+      ></todo-item>
     </transition-group>
     <div class="extra-container">
       <div>
@@ -56,8 +47,13 @@
 </template>
 
 <script>
+import TodoItem from "./TodoItem";
+
 export default {
   name: "todo-list",
+  components: {
+    TodoItem
+  },
   data() {
     return {
       newTodo: "",
@@ -100,13 +96,6 @@ export default {
       return this.todos.filter(todo => todo.completed).length > 0;
     }
   },
-  directives: {
-    focus: {
-      inserted: function(el) {
-        el.focus();
-      }
-    }
-  },
   methods: {
     addTodo() {
       if (this.newTodo.trim().length == 0) {
@@ -122,20 +111,6 @@ export default {
       this.newTodo = "";
       this.idForTodo++;
     },
-    editTodo(todo) {
-      this.beforeEditCache = todo.title;
-      todo.editing = true;
-    },
-    doneEdit(todo) {
-      if (this.newTodo.trim().length == 0) {
-        todo.title = this.beforeEditCache;
-      }
-      todo.editing = false;
-    },
-    cancelEdit(todo) {
-      todo.editing = false;
-      todo.title = this.beforeEditCache;
-    },
     removeTodo(index) {
       this.todos.splice(index, 1);
     },
@@ -146,6 +121,11 @@ export default {
     },
     clearCompleted() {
       this.todos = this.todos.filter(todo => !todo.completed);
+    },
+    finishedEdit(data) {
+        this.todos.splice(data.index, 1, data.todo)
+    //   const index = this.todos.findIndex(item => item.id == data.id);
+    //   this.todos.splice(index, 1, data);
     }
   }
 };
