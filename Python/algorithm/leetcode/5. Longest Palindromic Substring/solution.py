cefda86bcab3aaf1e9b9f0af537a1c72586a8f4f
Jiyeon.Park <40231980+Ji-yeonPark@users.noreply.github.com>
2019-06-28 16:20:37 +0900

    Update solution.py

--- Python/algorithm/leetcode/5. Longest Palindromic Substring/solution.py
+++ Python/algorithm/leetcode/5. Longest Palindromic Substring/solution.py
@@ -1,3 +1,4 @@
+# solution 01 : 6396 ms / 13.2 MB / python3
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         if not s or len(s) == 1 or s == s[::-1]:
@@ -11,3 +12,19 @@ class Solution:
                     palindrome = s[i:j]
                 j += 1
         return palindrome
+
+    
+
+# solution 02 : 4384 ms / 13.1 MB / python3
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s or len(s) == 1 or s == s[::-1]:
+            return s
+        
+        p = ""
+        for i in range(0, len(s)-1):
+            for j in range(i+1, len(s)+1):
+                str = s[i:j]
+                if str == str[::-1] and len(p) < len(str):
+                    p = str
+        return p
